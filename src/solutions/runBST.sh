dd3a16e9a163bd95d66fe614678e540a4bed1be5
J. van der WUlp <jeroen.van.der.wulp@sioux.eu>
2014-02-10 20:13:45 +0100

    BST: updated sh script to match the batch script...

--- src/solutions/runBST.sh
+++ src/solutions/runBST.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp BinarySearchTrees/classes_gen:QlRuntime/classes_gen BinarySearchTrees/Two/BST2
+java -cp BinarySearchTrees/classes_gen:QlRuntime/classes_gen BinarySearchTrees/Four/BST2
